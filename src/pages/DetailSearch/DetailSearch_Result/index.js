cf87b8f81a96bfc79dda59f0db3372a45dc34f54
HONG SE HYUN <sese0733@naver.com>
2023-10-17 14:02:10 +0900

    FEAT: 컴포넌트 모듈화

---
+++ src/pages/DetailSearch/DetailSearch_Result/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { DetailSearch_ImgContainer as ImgContainer } from './DetailSearch_ImgContainer'
+export const DetailSearch_Result = () => {
+    return (
+        <ImgContainer />
+    )
+}
